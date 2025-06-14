e6c7b7c0d421fab8b2f9266d844141178e16b7dc
Shree Hari N G <shreeharing20@gmail.com>
2025-06-14 21:42:38 +0530

    Fix: rename User.js to user.js

--- auth-sysytem/config/passport.js
+++ auth-sysytem/config/passport.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const bcrypt = require("bcrypt");
-const User = require("../models/user");
+const User = require("../models/User");
 passport.use(new LocalStrategy({
   usernameField: "email"
 }, async (email, password, done) => {
