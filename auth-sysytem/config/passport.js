f4f89b5edc13fb0f5d79f679d9ab8519d28cda22
Shree Hari N G <shreeharing20@gmail.com>
2025-06-14 21:58:53 +0530

    Fix: rename User.js to user.js

--- auth-sysytem/config/passport.js
+++ auth-sysytem/config/passport.js
@@ -3,6 +3,9 @@ const LocalStrategy = require("passport-local").Strategy;
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+dotenv.config();
+require("./config/passport");
+
 passport.use(new LocalStrategy({
   usernameField: "email"
 }, async (email, password, done) => {
@@ -33,7 +36,7 @@ passport.use(new LocalStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "https://wardrobemanagement.onrender.com/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ googleId: profile.id });
   if (!user) {
